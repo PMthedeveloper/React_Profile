a16e3842d93223c07805386ce620c5df50b6ffb3
PMthedeveloper <pmportfollio@gmail.com>
2021-02-25 16:05:12 +0530

    Updates

---
+++ precache-manifest.6aba28b3043f9f670155eb1a35350cf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8a6cfd34582bacb707bea484e8539c",
+    "url": "/React_Profile/index.html"
+  },
+  {
+    "revision": "6d3e1fb71c0e13288614",
+    "url": "/React_Profile/static/css/2.c8df05e9.chunk.css"
+  },
+  {
+    "revision": "316f4d3cc9e5d242031f",
+    "url": "/React_Profile/static/css/main.a73a1eea.chunk.css"
+  },
+  {
+    "revision": "6d3e1fb71c0e13288614",
+    "url": "/React_Profile/static/js/2.6594983f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React_Profile/static/js/2.6594983f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316f4d3cc9e5d242031f",
+    "url": "/React_Profile/static/js/main.02da3377.chunk.js"
+  },
+  {
+    "revision": "289e0207a3f2fde5d4c0",
+    "url": "/React_Profile/static/js/runtime-main.52ddcce7.js"
+  }
+]);
